d28ac67446e917a072cc88d5235b266d141aa7f9
Sophia Saiada <sophia.saiada@gmail.com>
2020-10-15 19:21:16 +0300

    Updates

---
+++ precache-manifest.cecf2aaf660b7d677c84cadbed993052.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3449793be8a2e1ff11e68d26e1af495",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "ba508888b2a0e884024d",
+    "url": "/app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "315fc049c4a53d700182",
+    "url": "/app/static/js/2.419ff7f1.chunk.js"
+  },
+  {
+    "revision": "b68ea9bfcdfe88fd25eac9df29279526",
+    "url": "/app/static/js/2.419ff7f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba508888b2a0e884024d",
+    "url": "/app/static/js/main.d156dac8.chunk.js"
+  },
+  {
+    "revision": "d278979d7c2cfbdf9d3f",
+    "url": "/app/static/js/runtime-main.929b948e.js"
+  },
+  {
+    "revision": "671ad7925a9f83d2258730de3ccc3fbf",
+    "url": "/app/static/media/01.671ad792.svg"
+  },
+  {
+    "revision": "8375b02f82ce755fc2b957ad52eb1a81",
+    "url": "/app/static/media/01.8375b02f.svg"
+  },
+  {
+    "revision": "1bc82864658eae5b109d83fcd1da7812",
+    "url": "/app/static/media/02.1bc82864.svg"
+  },
+  {
+    "revision": "ac8d05b0c219128e08e0725952ad70d3",
+    "url": "/app/static/media/02.ac8d05b0.svg"
+  },
+  {
+    "revision": "0186f2430ac2802713285841cb2d6492",
+    "url": "/app/static/media/03.0186f243.svg"
+  },
+  {
+    "revision": "811f31c5538bfef7dae83f71765e5a37",
+    "url": "/app/static/media/03.811f31c5.svg"
+  },
+  {
+    "revision": "02d7d336d73813ac5414d41bd38556b2",
+    "url": "/app/static/media/04.02d7d336.svg"
+  },
+  {
+    "revision": "faaaae74fe547b36f6c0e7150faf97b5",
+    "url": "/app/static/media/05.faaaae74.svg"
+  },
+  {
+    "revision": "bee4c83a60b81172489f9b7538f33ecd",
+    "url": "/app/static/media/06.bee4c83a.svg"
+  },
+  {
+    "revision": "6a18e736a507794c29bd80caa51677ad",
+    "url": "/app/static/media/07.6a18e736.svg"
+  },
+  {
+    "revision": "fce20ceceb280918244304c4c3334c49",
+    "url": "/app/static/media/08.fce20cec.svg"
+  },
+  {
+    "revision": "f5cc6d2eb13bc256d8aa88d0a5ce1c0e",
+    "url": "/app/static/media/CircuitBoard.f5cc6d2e.svg"
+  },
+  {
+    "revision": "1df3ce22471e1ec77b8859ec08079a43",
+    "url": "/app/static/media/EmptyState.1df3ce22.svg"
+  },
+  {
+    "revision": "2925106fb92dfd691a4ba05635570fb8",
+    "url": "/app/static/media/FacebookIcon.2925106f.svg"
+  },
+  {
+    "revision": "3ec228d3193537d0fb027a5f0a0a0a79",
+    "url": "/app/static/media/GoogleIcon.3ec228d3.svg"
+  },
+  {
+    "revision": "92c7842cc0a7bf1689fca6cc26409fb2",
+    "url": "/app/static/media/Logo.92c7842c.jpeg"
+  }
+]);
